1975997838a2a1519286f822ae97a91342ace083
Michael Pfammatter <pfammatter@gmail.com>
2022-01-29 14:44:07 -0600

    Macros: Add shortcuts to align windows

--- macros/mac-default.py
+++ macros/mac-default.py
@@ -21,14 +21,23 @@ app = {
             -Keycode.ENTER
         ]),
         (0x104000, 'RayC', [Keycode.COMMAND, Keycode.SPACE]),
-        (0x000000, '', ''),
+        (0xFF8000, '1/3', [
+            Keycode.COMMAND,
+            Keycode.SPACE,
+            -Keycode.SPACE,
+            -Keycode.COMMAND,
+            0.1,
+            'First Third',
+            Keycode.ENTER,
+            -Keycode.ENTER
+        ]),
         # 2nd row ----------
         (0x464eb8, 'Teams', [
             Keycode.COMMAND,
             Keycode.SPACE,
             -Keycode.SPACE,
-            0.1,
             -Keycode.COMMAND,
+            0.1,
             'microsoft teams',
             Keycode.ENTER,
             -Keycode.ENTER
@@ -36,14 +45,23 @@ app = {
         (0x404000, 'Char', [
             Keycode.CONTROL, Keycode.COMMAND, Keycode.SPACE
         ]),
-        (0x000000, '', ''),
+        (0xFF4400, '2/3', [
+            Keycode.COMMAND,
+            Keycode.SPACE,
+            -Keycode.SPACE,
+            -Keycode.COMMAND,
+            0.1,
+            'Last Two Thirds',
+            Keycode.ENTER,
+            -Keycode.ENTER
+        ]),
         # 3rd row ----------
         (0xa000a0, 'FFPri', [
             Keycode.COMMAND,
             Keycode.SPACE,
             -Keycode.SPACE,
-            0.1,
             -Keycode.COMMAND,
+            0.1,
             'firefox',
             Keycode.ENTER,
             -Keycode.ENTER,
@@ -56,19 +74,18 @@ app = {
         (0x000000, '', ''),
         # 4th row ----------
         (0x880000, 'Mute', [
-                Keycode.COMMAND,
-                Keycode.SPACE,
-                -Keycode.SPACE,
-                0.1,
-                -Keycode.COMMAND,
-                'microsoft teams',
-                Keycode.ENTER,
-                -Keycode.ENTER,
-                Keycode.COMMAND,
-                Keycode.SHIFT,
-                Keycode.M
-            ]
-        ),
+            Keycode.COMMAND,
+            Keycode.SPACE,
+            -Keycode.SPACE,
+            -Keycode.COMMAND,
+            0.1,
+            'microsoft teams',
+            Keycode.ENTER,
+            -Keycode.ENTER,
+            Keycode.COMMAND,
+            Keycode.SHIFT,
+            Keycode.M
+        ]),
         (0x000000, '', ''),
         (0x000000, '', ''),
     ],
