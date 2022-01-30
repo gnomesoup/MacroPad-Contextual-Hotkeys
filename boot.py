aa67a5559cc3f7feee74948604eb931f01277766
Michael Pfammatter <pfammatter@gmail.com>
2022-01-30 08:52:31 -0600

    Only mount CIRCUITPY if 12th key held down

--- boot.py
+++ boot.py
@@ -1,4 +1,12 @@
+import board
+import digitalio
+import storage
 import usb_cdc
 print("boot")
 
 usb_cdc.enable(console=True, data=True)
+button = digitalio.DigitalInOut(board.KEY12)
+button.pull = digitalio.Pull.UP
+
+if button.value:
+    storage.disable_usb_drive()
